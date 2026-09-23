c641537c161b72b3220eef229b4774ac7d871f9b
gvinko <geradntash@gmail.com>
2026-09-24 07:45:29 +1000

    Make autosave debounce flushable and cancellable

--- src/lib/debounce.ts
+++ src/lib/debounce.ts
@@ -1,7 +1,31 @@
-export function debounce<Args extends unknown[]>(fn: (...args: Args) => void, waitMs: number) {
+export interface Debounced<Args extends unknown[]> {
+  (...args: Args): void;
+  flush: () => void;
+  cancel: () => void;
+}
+
+export function debounce<Args extends unknown[]>(fn: (...args: Args) => void, waitMs: number): Debounced<Args> {
   let timer: ReturnType<typeof setTimeout> | null = null;
-  return (...args: Args) => {
-    if (timer) clearTimeout(timer);
-    timer = setTimeout(() => fn(...args), waitMs);
+  let pendingArgs: Args | null = null;
+  const run = () => {
+    timer = null;
+    const args = pendingArgs;
+    pendingArgs = null;
+    if (args) fn(...args);
   };
+  const debounced = ((...args: Args) => {
+    pendingArgs = args;
+    if (timer) clearTimeout(timer);
+    timer = setTimeout(run, waitMs);
+  }) as Debounced<Args>;
+  debounced.flush = () => {
+    if (timer) clearTimeout(timer);
+    run();
+  };
+  debounced.cancel = () => {
+    if (timer) clearTimeout(timer);
+    timer = null;
+    pendingArgs = null;
+  };
+  return debounced;
 }
